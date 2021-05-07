76173eec0a2f1f5f900db15ad1e59d1104325673
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-07 16:48:32 +0530

    --- Auto-Git Commit ---

--- codes/mem.c
+++ codes/mem.c
@@ -1,4 +1,18 @@
-#include<unistd.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<
+#include<unistd.h> //for system calls like pid control, fork , pipe, I/O operations
+#include<stdio.h> // std in/out
+#include<stdlib.h> //collection of methods, mainly of mem allocation and atoi stoi etc
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    int *p=malloc(sizeof(int));
+    assert(p!=NULL);
+
+    while (1)
+    {
+        Spin(1);
+        printf("for PID:%d, p is pointing to address:%p",getpid(),p);
+        *p+=1;
+    }
+    return 0;
+}
