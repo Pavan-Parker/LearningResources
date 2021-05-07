6b29415eb094e89cb3d9ddfb13dd5037ed39a6d3
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-07 12:47:28 +0530

    --- Auto-Git Commit ---

---
+++ codes/common.h
@@ -0,0 +1,19 @@
+/*Include guards to avoid double file inclusion during traslation of preprocessors*/
+#ifndef __common_h__
+#define __common_h__
+
+#include<sys/time.h> //to monitor time to sleep
+#include<sys/stat.h>
+#include<assert.h> //assertion
+
+double GetTime()
+{
+    struct timeval t;
+    int rc=gettimeofday(&t,)
+
+    
+}
+
+
+
+#endif
